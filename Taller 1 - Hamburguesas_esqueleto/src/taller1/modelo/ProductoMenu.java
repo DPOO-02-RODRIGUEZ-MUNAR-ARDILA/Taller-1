8d49c3195f47c113aa565016cdf6d8838906409f
jarp_ <jarp_@157.253.169.150>
2022-02-15 12:23:20 -0500

    avances

--- Taller 1 - Hamburguesas_esqueleto/src/taller1/modelo/ProductoMenu.java
+++ Taller 1 - Hamburguesas_esqueleto/src/taller1/modelo/ProductoMenu.java
@@ -1,7 +1,27 @@
 package taller1.modelo;
 
-public class ProductoMenu {
+public class ProductoMenu implements Producto{
 	private String nombre;
 	private int precioBase;
+	
+	public ProductoMenu(String nombre, int precio) {
+		this.nombre=nombre;
+		precioBase=precio;
+	}
+	
+	public int getPrecio() {
+		// TODO Auto-generated method stub
+		return precioBase;
+	}
+	
+	public String getNombre() {
+		// TODO Auto-generated method stubg
+		return nombre;
+	}
+	
+	public String generarTextoFactura() {
+		// TODO Auto-generated method stub
+		return nombre + "     "+ precioBase;
+	}
 
 }
